5cf2213df4514836e9e05029e7d8eb628f5ff9c7
Līga <asikeligalillija@gmail.com>
2025-12-08 11:36:54 +0200

    ty, Diego...

--- Main.java
+++ Main.java
@@ -1,40 +1,51 @@
 import java.util.Scanner;
+import java.lang.Math;
 
+public class MyClass {
 
-public class Main{
-public static void main(String[] args){
+public static void main(String args[]){
 
-    final byte months = 12;
-    final byte percent = 100;
-    int creditSum = -1;
-    float yearlyPercent = -1;
-    int paybackTime = -1;
+System.out.println("Loan calculator");
 
-    System.out.println("Kredīta kalkulātors");
-    var scanner = new Scanner(System.in);
-        while(true){
-        System.out.println("Ievadat kredīta summu : ");
-        creditSum = scanner.nextInt();
-        if (creditSum >= 1000 && creditSum <= 1000000)
-        break;
-        }
-        while(true){
-        System.out.println("Ievadat gada procentu likmi : ");
-        yearlyPercent = scanner.nextFloat();
-        if (yearlyPercent > 0 && yearlyPercent < 20)
-        break;
-        }
-        while(true){
-        System.out.println("Ievadat atmaksas termiņu gados : ");
-        paybackTime = scanner.nextInt();
-        if (paybackTime >= 0 && paybackTime < 30)
-        break;
-        }
-        float c = yearlyPercent / percent / months;
-        int n = paybackTime * months;
+int kreditSum = 0;
+float procentuLikme = 0.0f;
+int termins = 0;
 
-        double P = creditSum * (c * Math.pow((1 + c), n)) / (Math.pow((1 + c), n) - 1);
-        System.out.println("Lietotāja info : " + "Kredīta summa " + creditSum + " ;==; Procentu likme " + c + " ;==; Atmaksas termiņš " + n);
-        System.out.println("Ikmēneša maksājums : " + Math.round(P));
+procentuLikme = readInput("Loan amount: ", 1000, 1000000);
+procentuLikme = readInput("Yearly rate: ", 0, 20);
+termins = readInput("How many years: ", 1, 30);
+
+double monthlyPayment = calculateMortgage(termins, procentuLikme, kreditSum);
+System.out.println("Monthly pay: " + monthlyPayment + " EUR");
 }
+
+public static float readInput(String prompt, int loverLimit, int upperLimit){
+var scanner = new Scanner(System.in);
+float input = -1;
+while (true){
+System.out.println(prompt);
+input = scanner.nextFloat();
+
+if ( input >= 1000 && input <= 1000000){
+break;
+}
+
+System.out.println("The summ should be within the rules");
+}
+}
+
+public static double calculateMortgage(int termins, float procentuLikme, int kreditSum) {
+
+final int Percent = 100;
+final int MonthInYear = 12;
+
+int months = termins * MonthInYear;
+float rate = procentuLikme / Percent / MonthInYear;
+
+double izt1 = rate * Math.pow((1 + rate), months);
+double izt2 = Math.pow((1 + rate), months) - 1;
+double P = kreditSum * (izt1 / izt2);
+return P;
+}
+
 }
